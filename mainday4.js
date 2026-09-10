b48d20d5d90fcf2f09744e9081ea9195b0eceab5
Abhishek RR <reetharajesh0@gmail.com>
2026-09-10 18:59:15 +0530

    classwork/homeworkday 4

---
+++ mainday4.js
@@ -0,0 +1,6 @@
+name=require('./formatnameday4.js');
+obj=require('./userinfoday4.js');
+
+console.log(name)
+console.log(obj.hobby.toUpperCase())
+console.log(obj.hobby.length)
---
+++ mainnameday4.js
@@ -0,0 +1,8 @@
+b=require("./capitalizenameday4.js")
+c=require("./managerinfoday4.js")
+
+
+console.log(b)
+console.log(c.role.toUpperCase())
+console.log(c.role.length)
+console.log(c.role.search("inventory "))
